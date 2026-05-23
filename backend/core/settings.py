d9ac9d3f80e32149d8c1fb8275a2f415bf54bd8c
Sardorbek031 <w72075433@gmail.com>
2026-05-23 15:37:31 +0500

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -84,7 +84,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
